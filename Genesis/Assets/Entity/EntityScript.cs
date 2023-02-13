6c8549ade3be506223c675ad9ca3942c4ea25f8d
Eliott Mercier-Del-Forno <eliott.mercier-del-forno@epita.fr>
2023-02-13 11:45:53 +0100

    added health to entity

--- Genesis/Assets/Entity/EntityScript.cs
+++ Genesis/Assets/Entity/EntityScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EntityScript : MonoBehaviour
 {
     public float speed;
+    public int health;
     
     // Start is called before the first frame update
     void Start()
