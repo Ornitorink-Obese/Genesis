b7e42cbe326ad03b7c252e8243b15822904c5798
Yoann Silvain <yoann.silvain@epita.fr>
2023-03-05 14:37:02 +0100

    Ajout d'une barre de vie + système de coeurs pour se heal

--- Genesis/Assets/Entity/Player/PlayerScript.cs
+++ Genesis/Assets/Entity/Player/PlayerScript.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : EntityScript
 {
     public Rigidbody2D body;
+    public HealthBar healthBar;
     
-    // Start is called before the first frame update
+    public int maxHealth = 100;
+    public int currentHealth;
+    
+    
     void Start()
     {
         speed = 10;
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.UpArrow))
@@ -23,5 +26,29 @@ public class PlayerScript : EntityScript
             body.position += Vector2.left * (speed * Time.deltaTime);
         if (Input.GetKey(KeyCode.RightArrow))
             body.position += Vector2.right * (speed * Time.deltaTime);
+        
+        if (Input.GetKeyDown(KeyCode.H))
+            TakeDamage(20);
+    }
+
+    void TakeDamage(int damage)
+    {
+        if (currentHealth != 0)
+        {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+        }
+    }
+
+    public bool HealPlayer(int heal)
+    {
+        if (currentHealth != maxHealth)
+        {
+            currentHealth += heal;
+            healthBar.SetHealth(currentHealth);
+            return true;
+        }
+
+        return false;
     }
 }
---
+++ Genesis/Assets/HealPowerUp.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class HealPowerUp : MonoBehaviour
+{
+    public PlayerScript playerScript;
+    public int Heal = 20;
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            if (playerScript.HealPlayer(Heal))
+                Destroy(gameObject);
+        }
+    }
+}
