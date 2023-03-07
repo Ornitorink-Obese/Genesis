f5e4aeafb5d7e8f7b857d07bfcb9818c519fe9b1
Daalion <leojacquemet@gmail.com>
2023-03-07 10:41:38 +0100

    ajout du mob

--- Genesis/Assets/Entity/Mob/MobScript.cs
+++ Genesis/Assets/Entity/Mob/MobScript.cs
@@ -5,14 +5,62 @@ using UnityEngine;
 public class MobScript : EntityScript
 {
     // Start is called before the first frame update
+    public int damage;
+    public Rigidbody2D bob;
+    public PlayerScript joueur;
+
+
     void Start()
     {
-        
+        health = 50;
+        damage = 10;
+        speed = 5;
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        if (Vector2.Distance(transform.position,bob.position) < 5.0f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, bob.position , speed * Time.deltaTime);
+        }
         
+        if (health <= 0)
+        {
+            Destroy(transform.gameObject);
+        }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            joueur.health = joueur.health - damage;
+            Vector2 back = transform.position;
+            if(bob.position.x < transform.position.x)
+            {
+                back.x = back.x - 1;
+            }
+
+            else
+            {
+                back.x = back.x - 1;
+            }
+
+            transform.position = Vector2.MoveTowards(transform.position, back , speed * Time.deltaTime);
+            StopAllCoroutines();
+            Time.timeScale = 5f;
+            StartCoroutine(Waitfor());
+        }
+ 
+    }
+
+    IEnumerator Waitfor()
+    {
+        Debug.Log(Time.time);
+        yield return new WaitForSecondsRealtime(50f);
+        Debug.Log(Time.time);
+    }
+
 }
