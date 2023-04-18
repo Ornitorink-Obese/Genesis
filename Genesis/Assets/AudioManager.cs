e654ecc7de2a4541d8b93b2f721bdcfd501e4c77
Yoann Silvain <yoann.silvain@epita.fr>
2023-04-18 11:38:36 +0200

    ajout musiques

---
+++ Genesis/Assets/AudioManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] playlist;
+    public AudioSource Source;
+    private int index;
+    
+    void Start()
+    {
+        Source.clip = playlist[0];
+        Source.Play();
+    }
+
+    void Update()
+    {
+        if (!Source.isPlaying)
+        {
+            Source.clip = playlist[(index + 1) % playlist.Length];
+            Source.Play();
+        }
+    }
+}
