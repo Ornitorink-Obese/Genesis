6b05a15df3cf33b39273f6628739f3ccf1f46fc3
Daalion <leojacquemet@gmail.com>
2023-05-25 18:58:07 +0100

    MAJ mob

--- Genesis/Assets/Entity/Mob/AngeScript.cs
+++ Genesis/Assets/Entity/Mob/AngeScript.cs
@@ -10,26 +10,74 @@ public class AngeScript : MobScript
         health = 20;
         damage = 5;
         speed = 8;
+        atak = true;
+        charge = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (havetarget)
+        if (havetarget && charge && infeinte == false && KB == false)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position , speed * Time.deltaTime);
+            Vector2 direction = target.transform.position - transform.position;
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+            nf = UnityEngine.Random.Range(0,10000);
+            if (nf == 2000)
+            {
+                infeinte = true;
+                feinte = transform.position;
+                print("feinte");
+                int howf = UnityEngine.Random.Range(0,2);
+                if (howf == 0)
+                {
+                    feinte.y = feinte.y + 3;
+                    feinte.y--;
+                }
+
+                else
+                {
+                    feinte.y = feinte.y - 3;
+                    feinte.y--;
+                }
+            }
         }
 
-        if(target.IsTouching(mobCollider) )
+        if(KB)
         {
-            StartCoroutine(Waitfor());
+            transform.position = Vector2.MoveTowards(transform.position, recule, speed * Time.deltaTime);
+            if(Vector2.Distance(transform.position, recule) == 0)
+            {
+                KB = false;
+                print("BOUM!");
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.K))
-            health = 0;
+
+        if (infeinte)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, feinte, speed * Time.deltaTime);
+            if (Vector2.Distance(transform.position,feinte) == 0)
+            {
+                infeinte = false;
+                print("exit feinte");
+            }
+        }
+
+        
+        if (havetarget && charge == false && infeinte == false && KB == false)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, back, speed * Time.deltaTime);
+            if(Vector2.Distance(transform.position, back) == 0)
+            {
+                charge = true;
+                StartCoroutine(Waitfor());
+            }
+        }
+
         if (health <= 0)
         {
             Destroy(transform.gameObject);
+            ItemDrop();
         }
     }
 }
--- Genesis/Assets/Entity/Mob/BourreauScript.cs
+++ Genesis/Assets/Entity/Mob/BourreauScript.cs
@@ -10,11 +10,140 @@ public class BourreauScript : MobScript
         health = 100;
         damage = 40;
         speed = 2;
+        atak = true;
+        charge = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (havetarget && charge && infeinte == false && KB == false)
+        {
+            Vector2 direction = target.transform.position - transform.position;
+            transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+            nf = Random.Range(0,10000);
+            if (nf == 2000)
+            {
+                infeinte = true;
+                feinte = transform.position;
+                print("feinte");
+                int howf = Random.Range(0,2);
+                if (howf == 0)
+                {
+                    feinte.y = feinte.y + 3;
+                    feinte.y--;
+                }
+
+                else
+                {
+                    feinte.y = feinte.y - 3;
+                    feinte.y--;
+                }
+            }
+        }
+
+        if(KB)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, recule, speed * Time.deltaTime);
+            if(Vector2.Distance(transform.position, recule) == 0)
+            {
+                KB = false;
+                print("BOUM!");
+            }
+        }
+
+
+        if (infeinte)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, feinte, speed * Time.deltaTime);
+            if (Vector2.Distance(transform.position,feinte) == 0)
+            {
+                infeinte = false;
+                print("exit feinte");
+            }
+        }
+
         
+        if (havetarget && charge == false && infeinte == false && KB == false)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, back, speed * Time.deltaTime);
+            if(Vector2.Distance(transform.position, back) == 0)
+            {
+                charge = true;
+                StartCoroutine(Waitfor());
+            }
+        }
+
+        if (health <= 0)
+        {
+            Destroy(transform.gameObject);
+            ItemDrop();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (collision is BoxCollider2D)
+            {
+                havetarget = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            if (collision is BoxCollider2D)
+            {
+                print("pastabox");
+                havetarget = true;
+                target = collision.gameObject;
+            }
+
+            if (collision is CapsuleCollider2D)
+            {
+                HealthManager.instance.TakeDamage(damage);
+                print("charge");
+                charge = false;
+                back = transform.position;
+                if (target.transform.position.x < transform.position.x)
+                {
+                    back.x = back.x + 3;
+                    back.x--;
+                }
+
+                else
+                {
+                    back.x = back.x - 3;
+                    back.x--;
+                }
+                
+            }
+            else if (collision is CircleCollider2D && atak)
+            {
+                print("tatakae");
+                atak = false;
+                TakeDamage(PlayerScript.instance.damage);
+                KB = true;
+                recule = transform.position;
+                if (target.transform.position.x < transform.position.x)
+                {
+                    recule.x = recule.x + 2;
+                    recule.x--;
+                }
+
+                else
+                {
+                    recule.x = recule.x - 2;
+                    recule.x--;
+                }
+                StartCoroutine(Wait(2));
+                atak = true;
+            }
+        }
     }
 }
